27dcfaa9307c2ed15ae87dd18e0f68cb95dd7f45
Mahmoud <naggar218@gmail.com>
2017-08-27 00:34:49 +0200

    http client setup complete and tested

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
 
 namespace csharp_rest_client
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      ProcessRepos().Wait();
     }
+
+    private static async Task ProcessRepos()
+    {
+      var client = new HttpClient();
+      client.DefaultRequestHeaders.Accept.Clear();
+      client.DefaultRequestHeaders.Accept.Add(
+        new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json")
+        );
+      client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+      var serializer = new DataContractJsonSerializer(
+        typeof(List<repo>)
+        );
+
+      var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+
+      var repositories = serializer.ReadObject(await streamTask) as List<repo>;
+
+      foreach (var repo in repositories)
+      {
+        Console.WriteLine(repo.name);
+      }
+    }
+  }
 }
---
+++ repo.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace csharp_rest_client
+{
+  public class repo
+  {
+    public string name;
+  }
+}
