1c7eb32cbe04990459683d296058aad077d5b5db
Mahmoud <naggar218@gmail.com>
2017-08-26 23:36:07 +0200

    first commit for initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_rest_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
